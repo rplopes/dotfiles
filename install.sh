42cabe612b372843b1ea176e1f7c02fada8f7442
Ricardo Lopes <mail@ricardolopes.net>
2024-06-27 09:51:32 +0100

    Fix install script by loading Nix before installing home-manager

--- install.sh
+++ install.sh
@@ -5,5 +5,8 @@
 # Install Nix
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
 
+# Load Nix
+. /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+
 # Install home-manager
 nix run home-manager/master -- init --switch
